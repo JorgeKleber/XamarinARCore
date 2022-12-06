cd55aea14527dc36eb73087dddb0cffed197461c
Jorge Kleber <jorge_kleber@outlook.com>
2022-12-06 11:49:42 +0000

    Verificando o dispositivo.
    - Importando xamarin.google.ARCore.
    - Configurando o projeto (Manifest).
    - Verificando se o dispositivo é compatível com o ARCore.

---
+++ XamarinARCore/Controller/ARCoreController.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Google.AR.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XamarinARCore.Controller
+{
+	public class ARCoreController
+	{
+		private Context context;
+
+		public ARCoreController(Context contextActivity)
+		{
+			context = contextActivity;
+		}
+
+
+		public bool CheckARcore()
+		{
+			ArCoreApk.Availability isARCoreAvaliable = ArCoreApk.Instance.CheckAvailability(context);
+
+			return isARCoreAvaliable.IsSupported;
+		}
+	}
+}
--- XamarinARCore/MainActivity.cs
+++ XamarinARCore/MainActivity.cs
@@ -1,20 +1,42 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
+using XamarinARCore.Controller;
 
 namespace XamarinARCore
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private ARCoreController controllerAR;
+        private TextView statusARcore;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            controllerAR = new ARCoreController(this);
+
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            statusARcore = FindViewById<TextView>(Resource.Id.tv_status_ar);
+
+            bool isARCoreAvaliable = controllerAR.CheckARcore();
+
+
+            if (isARCoreAvaliable)
+			{
+                statusARcore.Text = "ARCore is avaliable for this device!";
+            }
+			else
+			{
+                statusARcore.Text = "ARCore is NOT avaliable for this device!";
+            }
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
